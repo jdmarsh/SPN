f7f5e9cee01eaf6cd7aa36f2e9bf0639a01c083b
jdmarsh <jdmarsh@outlook.com>
2014-04-25 21:01:29 +0100

    Simplified Vectors

--- SPN/Main.cpp
+++ SPN/Main.cpp
@@ -1,7 +1,7 @@
 #include "Main.h"
 
 int main() {
-    Text pt{ { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+    Text pt{ 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1 };
     for (unsigned i = 0; i < 3; ++i) {
         pt.keyxor(i);
         pt.Sbox();
@@ -10,6 +10,7 @@ int main() {
     pt.keyxor(3);
     pt.Sbox();
     pt.keyxor(4);
-    std::cout << pt.btou(0) << "," << pt.btou(1) << "," << pt.btou(2) << "," << pt.btou(3) << std::endl;
+    std::cout << pt.btou(0) << "," << pt.btou(1) << ","
+        << pt.btou(2) << "," << pt.btou(3) << std::endl;
     return 0;
 }
