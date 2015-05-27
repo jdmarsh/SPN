34cd0c1dd0d8cb96a8be0d4766c3b56d84192438
James Marsh <james.marsh@purplesecure.com>
2015-05-27 10:32:45 +0100

    Now using better arguments

--- SPN/Main.cpp
+++ SPN/Main.cpp
@@ -1,18 +1,11 @@
 #include "main.h"
 
 int main() {
-    text pt{ 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1 };
-    for (unsigned i = 0; i < 3; ++i) {
-        pt.xor_key(i);
-        pt.substitution_box();
-        pt.permutation_box();
-    }
-    pt.xor_key(3);
-    pt.substitution_box();
-    pt.xor_key(4);
+    text spn("11100111011001111001000000111101");
+    std::string encryptedMessage = spn.encryptMessage("0100111010100001", 3);
 
-    std::cout << pt.bits_to_unsigned(0) << "," << pt.bits_to_unsigned(1) << ","
-        << pt.bits_to_unsigned(2) << "," << pt.bits_to_unsigned(3)
+    std::cout << spn.bits_to_unsigned(encryptedMessage, 0) << "," << spn.bits_to_unsigned(encryptedMessage, 1) << ","
+        << spn.bits_to_unsigned(encryptedMessage, 2) << "," << spn.bits_to_unsigned(encryptedMessage, 3)
         << std::endl;
 
     return 0;
--- SPN/Main.h
+++ SPN/Main.h
@@ -1,74 +1,110 @@
-#pragma once
 #ifndef SPN_MAIN_H
 #define SPN_MAIN_H
 
 #include <iostream>
-#include <vector>
-#include <array>
+#include <string>
+
+unsigned ascii2decimal(char);
 
 class text {
 public:
-    text(std::initializer_list<unsigned>);
-    unsigned bits_to_unsigned(unsigned);
-    void xor_key(unsigned);
-    void substitution_box();
-    void permutation_box();
+    text(std::string);
+    text(std::string, std::string, std::string);
+    std::string encryptMessage(std::string, unsigned);
+    unsigned bits_to_unsigned(std::string&, unsigned);
 
 private:
-    static const std::array<unsigned, 16> SB;
-    static const std::array<unsigned, 16> PB;
+    void xor_key(std::string&, unsigned);
+    void substitution_box(std::string&);
+    void permutation_box(std::string&);
 
-    std::vector<unsigned> key;
-    std::vector<unsigned> bits;
+    std::string key;
+    std::string SB;
+    std::string PB;
 };
 
-const std::array<unsigned, 16> text::SB = { 4, 1, 14, 8, 13, 6, 2, 11,
-                                            15, 12, 9, 7, 3, 10, 5, 0 };
-const std::array<unsigned, 16> text::PB = { 0, 4, 8, 12, 1, 5, 9, 13,
-                                            2, 6, 10, 14, 3, 7, 11, 15 };
+text::text(std::string binaryKey) {
+    //align key to 16 bits
+    key = binaryKey;
+    SB = "41e8d62bfc973a50";
+    PB = "048c159d26ae37bf";
+}
 
-text::text(std::initializer_list<unsigned> list)
-{
-    for (auto it : list) {
-        bits.emplace_back(it);
+text::text(std::string binaryKey, std::string subBox, std::string permBox) {
+    key = binaryKey;
+    SB = subBox;
+    PB = permBox;
+};
+
+std::string text::encryptMessage(std::string message, unsigned itterations) {
+    //align message to 16 bits
+    for (unsigned i = 0; i < itterations; ++i) {
+        xor_key(message, i);
+        substitution_box(message);
+        permutation_box(message);
     }
-    key = { 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1,
-    1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1 };
+    xor_key(message, itterations);
+    substitution_box(message);
+    xor_key(message, itterations+1);
+    return message;
 }
 
-unsigned text::bits_to_unsigned(unsigned i)
-{
-    return (bits[i * 4] * 8) + (bits[(i * 4) + 1] * 4) +
-        (bits[(i * 4) + 2] * 2) + bits[(i * 4) + 3];
-}
-
-void text::xor_key(unsigned round)
-{
-    for (unsigned i = 0; i < 16; ++i) {
-        bits[i] ^= key[(round * 4) + i];
-    }
-}
-
-void text::substitution_box()
-{
-    for (unsigned i = 0; i < 4; ++i) {
-        unsigned x = SB[bits_to_unsigned(i)];
-        bits[i*4] = (x & 0x8) >> 3;
-        bits[(i*4)+1] = (x & 0x4) >> 2;
-        bits[(i*4)+2] = (x & 0x2) >> 1;
-        bits[(i*4)+3] = x & 0x1;
-    }
-}
-
-void text::permutation_box()
-{
-    for (unsigned i = 0; i < 16; ++i) {
-        if (i <= PB[i]) {
-            unsigned temp = bits[i];
-            bits[i] = bits[PB[i]];
-            bits[PB[i]] = temp;
+unsigned text::bits_to_unsigned(std::string& message, unsigned i) {
+    unsigned result = 0;
+    for (unsigned index = (i * 4); index < (i * 4) + 4; ++index) {
+        result = result << 1;
+        if (message[index] == '1') {
+            ++result;
         }
     }
+    return result;
+}
+
+void text::xor_key(std::string& message, unsigned round) {
+    for (unsigned i = 0; i < 16; ++i) {
+        bool messageBit = (message[i] == '1');
+        bool keyBit = (key[(round * 4) + i] == '1');
+        if (messageBit ^ keyBit) {
+            message[i] = '1';
+        } else {
+            message[i] = '0';
+        }
+    }
+}
+
+void text::substitution_box(std::string& message) {
+    for (unsigned i = 0; i < 4; ++i) {
+        unsigned x = ascii2decimal(SB[bits_to_unsigned(message, i)]);
+        for (unsigned j = 0; j < 4; ++j) {
+            if (((x >> (3 - j)) & 0x1) > 0) {
+                message[(i * 4) + j] = '1';
+            } else {
+                message[(i * 4) + j] = '0';
+            }
+        }
+    }
+}
+
+void text::permutation_box(std::string& message) {
+    for (unsigned i = 0; i < 16; ++i) {
+        if (i <= ascii2decimal(PB[i])) {
+            unsigned temp = message[i];
+            message[i] = message[ascii2decimal(PB[i])];
+            message[ascii2decimal(PB[i])] = temp;
+        }
+    }
+}
+
+unsigned ascii2decimal(char character) {
+    unsigned decimal = 0;
+    if (character >= '0' && character <= '9') {
+        decimal = static_cast<unsigned>(character - 48);
+    } else if (character >= 'A' && character <= 'F') {
+        decimal = static_cast<unsigned>(character - 55);
+    } else if (character >= 'a' && character <= 'f') {
+        decimal = static_cast<unsigned>(character - 87);
+    }
+    return decimal;
 }
 
 #endif // SPN_MAIN_H
